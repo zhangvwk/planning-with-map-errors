86f5388de53b3fa1d7d52074b1532981416909da
zhangvwk <zhangvwk@stanford.edu>
2020-05-10 22:45:07 -0700

    Compute LQR gain at init + cleaning

--- lqr.py
+++ lqr.py
@@ -1,13 +1,15 @@
+# Standard libraries
 import numpy as np
 from scipy.linalg import solve_discrete_are
 
 
 class LQRPlanner:
-    def __init__(self, Q=0, R=0, A=0, B=np.array([0,0]).reshape(2,1)):
-        """LQRPlanner constructor
+    """Class for computing the optimal trajectory and cost between
+    two points, assuming infinite horizon discrete LQR.
+    """
 
-        Default arguments are meaningless and left here for
-        backward compatibility.
+    def __init__(self, Q, R, A, B):
+        """LQRPlanner constructor
 
         Args:
             Q: state cost (NxN)
@@ -17,22 +19,27 @@ class LQRPlanner:
         """
         Ncontrol = B.shape[1]
         self.Q = Q
-        if Ncontrol==1:
+        if Ncontrol == 1:
             self.R = np.array(R).reshape((Ncontrol,))
         else:
             self.R = R
         self.A = A
         self.B = B
+        self.compute_gain()  # compute and store the gain at initialization
 
+    def compute_gain(self):
+        """Solve infinite horizon discrete ARE."""
+        V = solve_discrete_are(self.A, self.B, self.Q, self.R)
+        self.lqr_gain = np.linalg.inv(self.R + self.B.T.dot(V.dot(self.B)))
+        self.lqr_gain = self.lqr_gain.dot(self.B.T.dot(V.dot(self.A)))
 
-    def compute_path(self, u, v):
-        """ Left here for backward compatibility."""
-        cost = np.linalg.norm(u - v)
-        path = np.vstack((u, v))
-        return cost, path
+    def get_gain(self):
+        """Return the LQR gain.
+        Meant to be called outside the class.
+        """
+        return self.lqr_gain
 
-
-    def compute_path_new(self, u, v, max_iter=1000, tol=1e-6):
+    def compute_path(self, u, v, max_iter=1000, tol=1e-6):
         """Compute a path from u to v using an LQR regulator
 
         Args:
@@ -41,34 +48,41 @@ class LQRPlanner:
             max_iter: maximum iteration before stopping forward path
             tol: termination criterion for forward pass
         """
-        # solve infinite horizon discrete ARE
-        V = solve_discrete_are(self.A, self.B, self.Q, self.R)
-        lqr_gain = np.linalg.inv(self.R + self.B.T.dot(V.dot(self.B)))
-        lqr_gain = lqr_gain.dot(self.B.T.dot(V.dot(self.A)))
-
         cost = 0
-        path = [u-v]
+        path = [u - v]
         done = False
         converged = False
         iteration = 0
 
         while not done:
             x = path[iteration]
-            control = -lqr_gain.dot(x)
-            cost += 0.5*x.T.dot(self.Q.dot(x)) + 0.5*control.T.dot(self.R.dot(control))
+            control = -self.lqr_gain.dot(x)
+            cost += 0.5 * x.T.dot(self.Q.dot(x)) + 0.5 * control.T.dot(
+                self.R.dot(control)
+            )
 
             x = self.A.dot(x) + self.B.dot(control)
             path.append(x)
             iteration += 1
 
-            converged = np.linalg.norm(x)<tol
-            done = converged or (iteration>=max_iter)
+            converged = np.linalg.norm(x) < tol
+            done = converged or (iteration >= max_iter)
 
         if not converged:
             cost = np.Inf
 
         Nsteps = len(path)
         Nstate = u.shape[0]
-        path =  np.vstack(path).reshape(Nsteps, Nstate)
+        path = np.vstack(path).reshape(Nsteps, Nstate)
         path += v.reshape(1, Nstate)
+        return cost, path
+
+
+class DummyPlanner:
+    def __init__(self):
+        pass
+
+    def compute_path(self, u, v):
+        cost = np.linalg.norm(u - v)
+        path = np.vstack((u, v))
         return cost, path
