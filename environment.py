d21c56cfd6b0bb0e6b0ba9bc001fcb1384497f99
zhangvwk <zhangvwk@stanford.edu>
2020-05-16 18:10:34 -0700

    Add add_obstacles method for convenience

--- environment.py
+++ environment.py
@@ -1,5 +1,10 @@
 # Standard libraries
+import numpy as np
 import matplotlib.pyplot as plt
+
+# Custom libraries
+from shapes import Rectangle
+from utils import GeoTools
 
 
 class Environment2D:
@@ -9,8 +14,7 @@ class Environment2D:
     """
 
     def __init__(self, x_lims, y_lims):
-        self.x_lims = x_lims
-        self.y_lims = y_lims
+        self.x_range = np.array([x_lims, y_lims])
         self.initialize_rectangles()
 
     def initialize_rectangles(self):
@@ -19,28 +23,22 @@ class Environment2D:
     def clear(self):
         self.initialize_rectangles()
 
+    def as_rectangle(self):
+        """Return the environment as a Rectangle object."""
+        return Rectangle(
+            -1,
+            self.x_range[0][0],
+            self.x_range[1][0],
+            self.x_range[0][1] - self.x_range[0][0],
+            self.x_range[1][1] - self.x_range[1][0],
+            0,
+        )
+
     def add_rectangles(self, list_rectangles):
         for rectangle in list_rectangles:
             self.add_rectangle(rectangle)
 
-    def is_valid_id(self, rectangle):
-        return rectangle.id not in self.rectangles
-
-    def is_valid_rectangle(self, rectangle):
-        for i, rec in self.rectangles.items():
-            vol = rectangle.as_poly.intersect(rec.as_poly).volume
-            if vol < 1e-5:
-                continue
-            else:
-                print(
-                    "Rectangle collides with existing rectangle of ID {}.".format(
-                        rec.id
-                    )
-                )
-                return False
-        return True
-
-    def add_rectangle(self, rectangle):
+    def add_rectangle(self, rectangle, verbose=True):
         try:
             assert self.is_valid_id(rectangle)
         except AssertionError:
@@ -51,11 +49,63 @@ class Environment2D:
             )
             raise
         try:
-            assert self.is_valid_rectangle(rectangle)
+            assert self.is_valid_rectangle(rectangle, verbose)
             self.rectangles[rectangle.id] = rectangle
+            vertices = rectangle.get_vertices()
+            min_values = np.min(vertices, axis=0)
+            max_values = np.max(vertices, axis=0)
+            self.x_range[0][0], self.x_range[1][0] = (
+                min(self.x_range[0][0], min_values[0] - 1),
+                min(self.x_range[1][0], min_values[1] - 1),
+            )
+            self.x_range[0][1], self.x_range[1][1] = (
+                max(self.x_range[0][1], max_values[0] + 1),
+                max(self.x_range[1][1], max_values[1] + 1),
+            )
         except AssertionError:
-            print("Please choose another rectangle.")
+            if verbose:
+                print("Please choose another rectangle.")
             raise
+
+    def is_valid_id(self, rectangle):
+        return rectangle.id not in self.rectangles
+
+    def is_valid_rectangle(self, rectangle, verbose=True):
+        for i, rec in self.rectangles.items():
+            vol = rectangle.as_poly.intersect(rec.as_poly).volume
+            if vol < 1e-5:
+                continue
+            else:
+                if verbose:
+                    print(
+                        "Rectangle collides with existing rectangle of ID {}.".format(
+                            rec.id
+                        )
+                    )
+                return False
+        return True
+
+    def add_obstacles(self, num_obs, lgth, wdth, ang=None):
+        """Add num_obs obstacles of certain length and width, within
+        the limits of the current environment.
+        If ang is not specified, will pick a random rectangle angle.
+        """
+        max_id = max(self.rectangles)
+        added = 0
+        while added < num_obs:
+            x, y = GeoTools.sample_in_range(self.x_range)
+            if ang is not None:
+                rectangle = Rectangle(
+                    max_id + added + 1, x, y, lgth, wdth, np.random.uniform(0, 360)
+                )
+            else:
+                rectangle = Rectangle(max_id + added + 1, x, y, lgth, wdth, ang)
+            try:
+                assert rectangle.as_poly <= self.as_rectangle().as_poly
+                self.add_rectangle(rectangle, verbose=False)
+            except AssertionError:
+                continue
+            added += 1
 
     def contains(self, p):
         for rectangle_id in self.rectangles:
@@ -120,8 +170,8 @@ class Environment2D:
         return False
 
     def plot(self, figsize=(10, 10)):
-        plt.xlim(self.x_lims)
-        plt.ylim(self.y_lims)
+        plt.xlim(self.x_range[0])
+        plt.ylim(self.x_range[1])
         for rectangle_id, rectangle in self.rectangles.items():
             rectangle.plot()
         plt.gca().set_aspect("equal", adjustable="box")
